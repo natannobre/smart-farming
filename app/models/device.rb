f0bdeda60c3fd0dd2a86bdb9e50bfe20095c8a26
natannobre <natannobre37@gmail.com>
2022-10-26 14:31:47 -0300

    add method to device model

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,2 +1,9 @@
 class Device < ApplicationRecord
+  def self.entity_name(device)
+    "urn:ngsd-ld:#{device.device_id}"
+  end
+
+  def self.format_mqtt_topic(device)
+    "/ul/#{ENV['API_KEY']}/#{device.device_id}/attrs"
+  end
 end
