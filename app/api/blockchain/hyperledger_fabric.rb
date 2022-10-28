cd239e1247d0d7d44ade22c1c4bcfcd9d3f7148b
natannobre <natannobre37@gmail.com>
2022-10-28 02:11:01 -0300

    Add GET BY ID request to blockchain

--- app/api/blockchain/hyperledger_fabric.rb
+++ app/api/blockchain/hyperledger_fabric.rb
@@ -3,10 +3,10 @@ module Blockchain
     @base_url = 'http://host.docker.internal:4000'
 
     def self.fetch_by_device_id(device_id: 'temp001')
-      path = "/channels/mychannel/chaincodes/fabcar?fcn=GetCarById&args=[#{device_id}]"
+      path = "/channels/mychannel/chaincodes/fabcar?fcn=GetCarById&args=[\"#{device_id}\"]"
 
       HTTParty.get("#{@base_url}#{path}",
-                   headers: { 'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjY5NjI0OTMsInVzZXJuYW1lIjoibmF0YW5ub2JyZTEiLCJvcmdOYW1lIjoiT3JnMSIsImlhdCI6MTY2NjkyNjQ5M30.mVVzoYsrQaQTwuuF6sRzbznrWJkLbWpnt0efJe0t4cY' })
+                   headers: { Authorization: "Bearer #{ENV['BLOCKCHAIN_API_KEY']}" })
     end
   end
 end
